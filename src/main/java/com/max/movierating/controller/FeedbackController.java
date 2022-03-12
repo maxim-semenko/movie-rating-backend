02f8fbf3f35ca540928038d7aed38ab26d9e1229
pro100-Max <maks.semenko@gmail.com>
2022-03-13 00:18:25 +0300

    UPD version 3.8.1 Added new unit-tests Added new DTO classes Deleted Admin controller, service, interface Remade method update user's role and lock

--- src/main/java/com/max/movierating/controller/FeedbackController.java
+++ src/main/java/com/max/movierating/controller/FeedbackController.java
@@ -5,7 +5,6 @@ import com.max.movierating.dto.entity.RequestFeedbackDTO;
 import com.max.movierating.entity.Feedback;
 import com.max.movierating.entity.FeedbackType;
 import com.max.movierating.service.impl.FeedbackServiceImpl;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
